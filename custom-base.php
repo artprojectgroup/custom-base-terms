99bd63539259c0c2db6c8f16eb166208ef1a4f5e
artprojectgroup <artprojectgroup@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-23 02:51:11 +0000

    Versión 0.3
    git-svn-id: http://plugins.svn.wordpress.org/custom-base-terms/trunk@809153 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-base.php
+++ custom-base.php
@@ -1,16 +1,44 @@
 <?php
 /*
 Plugin Name: Custom Base Terms
-Version: 0.2
+Version: 0.3
 Plugin URI: http://wordpress.org/plugins/custom-base-terms/
-Description: Gracias a Custom Base Terms se podrán introducir las estructuras personalizadas en las URLs para autor, búsqueda, comentarios y página. Creado a partir del plugin de <a href="http://profiles.wordpress.org/jfarthing84/">Jeff Farthing</a> <a href="http://wordpress.org/plugins/custom-author-base/"><strong>Custom Author Base</strong></a>.
+Description: With Custom Base Terms you can create a custom structures for URLs in author, search, comments and page. Created from <a href="http://profiles.wordpress.org/jfarthing84/" target="_blank">Jeff Farthing</a> <a href="http://wordpress.org/plugins/custom-author-base/" target="_blank"><strong>Custom Author Base</strong></a> plugin.
 Author: Art Project Group
-Author URI: http://www.artprojectgroup.com/
+Author URI: http://www.artprojectgroup.es/
+
+Text Domain: custom_base_terms
+Domain Path: /lang
+License: GPL2
 */
 
-/**
- * Initializes the plugin
- */
+/*  Copyright 2013  artprojectgroup  (email : info@artprojectgroup.es)
+
+    This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License, version 2, as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+//Carga el idioma
+load_plugin_textdomain('custom_base_terms', null, dirname(plugin_basename(__FILE__)) . '/lang');
+
+//Enlaces adicionales personalizados
+function custom_base_terms_enlaces($enlaces, $archivo) {
+	$plugin = plugin_basename(__FILE__);
+
+	if ($archivo == $plugin) 
+	{
+		$enlaces[] = '<a href="http://www.artprojectgroup.es/plugins-para-wordpress/custom-base-terms" target="_blank" title="Art Project Group">' . __('Visit the official plugin website', 'custom_base_terms') . '</a>';
+		$enlaces[] = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EWFS5SMZ5UYU4" target="_blank" title="PayPal"><img alt="Custom Base Terms" src="' . __('https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif', 'custom_base_terms') . '" width="53" height="15" style="vertical-align:text-bottom;"></a>';
+	}
+		
+	return $enlaces;
+}
+add_filter('plugin_row_meta', 'custom_base_terms_enlaces', 10, 2);
+
+// Initializes the plugin
 $bases = array('author_base' => 'author', 'search_base' => 'search', 'comments_base' => 'comments', 'pagination_base' => 'page');
 
 function cbt_init() {
@@ -18,58 +46,50 @@ function cbt_init() {
 
 	foreach($bases as $base => $nombre)
 	{
-		$custom_base = get_option( $base );
-		$wp_rewrite->$base = empty( $custom_base ) ? mb_strtolower( __( ucfirst($nombre) ) ) : $custom_base;
+		$custom_base = get_option($base);
+		$wp_rewrite->$base = empty($custom_base) ? mb_strtolower(__(ucfirst($nombre))) : $custom_base;
 	}
 }
-add_action( 'init', 'cbt_init' );
+add_action('init', 'cbt_init');
 
-/**
- * Adds base terms fields to permalink settings page
- */
+// Adds base terms fields to permalink settings page
 function cbt_load_options_permalink() {
 	global $bases;
 
 	foreach($bases as $base => $nombre)
 	{
-		if ( isset( $_POST[$base] ) ) {
+		if (isset($_POST[$base])) {
 			$custom_base = $_POST[$base];
-			if ( !empty( $custom_base ) )
-				$custom_base = preg_replace( '#/+#', '/', '/' . $custom_base );
-			cbt_set_base( $custom_base, $base, $nombre );
+			if (!empty($custom_base)) $custom_base = preg_replace('#/+#', '/', '/' . $custom_base);
+			cbt_set_base($custom_base, $base, $nombre);
 		}
 
-		add_settings_field( $base, __( ucfirst($nombre) ) . ' base', 'cbt_settings_field',
-			'permalink', 'optional', array( 'label_for' => $base ) );
+		add_settings_field($base, __(ucfirst($nombre)) . ' base', 'cbt_settings_field', 'permalink', 'optional', array('label_for' => $base));
 	}
 }
-add_action( 'load-options-permalink.php', 'cbt_load_options_permalink' );
+add_action('load-options-permalink.php', 'cbt_load_options_permalink');
 
-/**
- * Displays base terms settings field
- */
+// Displays base terms settings field
 function cbt_settings_field($campo) {
 	$campo = $campo['label_for'];
-	echo '<input name="'.$campo.'" id="'.$campo.'" type="text" value="' . esc_attr( get_option( $campo ) ) . '" class="regular-text code" />';
+	echo '<input name="'.$campo.'" id="'.$campo.'" type="text" value="' . esc_attr(get_option($campo)) . '" class="regular-text code" />';
 }
 
-/**
- * Set the base for the terms permalink
- */
-function cbt_set_base ( $custom_base, $base, $nombre ) {
+// Set the base for the terms permalink
+function cbt_set_base ($custom_base, $base, $nombre) {
 	global $wp_rewrite;
 
-	if ( $custom_base != $wp_rewrite->$base ) {
-		update_option( $base, $custom_base );
+	if ($custom_base != $wp_rewrite->$base) 
+	{
+		update_option($base, $custom_base);
 		$wp_rewrite->init();
-		$wp_rewrite->$base = empty( $custom_base ) ? mb_strtolower( __( ucfirst($nombre) ) ) : $custom_base;
+		$wp_rewrite->$base = empty($custom_base) ? mb_strtolower(__(ucfirst($nombre))) : $custom_base;
 	}
 }
 
 // Filter the base terms
-add_filter( 'option_author_base', '_wp_filter_taxonomy_base' );
-add_filter( 'option_search_base', '_wp_filter_taxonomy_base' );
-add_filter( 'option_comments_base', '_wp_filter_taxonomy_base' );
-add_filter( 'option_pagination_base', '_wp_filter_taxonomy_base' );
-
+add_filter('option_author_base', '_wp_filter_taxonomy_base');
+add_filter('option_search_base', '_wp_filter_taxonomy_base');
+add_filter('option_comments_base', '_wp_filter_taxonomy_base');
+add_filter('option_pagination_base', '_wp_filter_taxonomy_base');
 ?>
--- readme.txt
+++ readme.txt
@@ -1,46 +1,88 @@
 === Custom Base Terms ===
 Contributors: artprojectgroup
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EWFS5SMZ5UYU4
-Tags: permalinks, author, search, comments, pagination
+Tags: permalinks, author, search, comments, pagination, page, sep, url friendly
 Requires at least: 2.7
-Tested up to: 3.5.2
-Stable tag: 0.2
+Tested up to: 3.7.1
+Stable tag: 0.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Modifique las estructuras personalizadas en las URLs para autor, búsqueda, comentarios y página.
 
 == Description ==
+**Custom Base Terms** genera cuatro nuevas opciones en *Ajustes -> Enlaces permanentes* donde se podrán introducir las estructuras personalizadas en las URLs para autor, búsqueda, comentarios y página.
 
-Gracias a **Custom Base Terms** aparecerán 4 nuevas opciones en Ajustes -> Enlaces permanentes donde se podrán introducir las estructuras personalizadas en las URLs para autor, búsqueda, comentarios y página.
+= Características =
+* Simple y sencillo de configurar.
+* Permite crear URLs Friendly.
+* Ayuda a mejorar el SEO de WordPress. 
 
-Puede obtener más información sobre **Custom Base Terms** en nuestro sitio web oficial: http://www.artprojectgroup.es/plugins-para-wordpress/custom-base-terms
+= Origen =
+**Custom Base Terms** ha sido programado a partir del plugin [*Custom Author Base*](http://wordpress.org/plugins/custom-author-base/) de [Jeff Farthing](http://profiles.wordpress.org/jfarthing84/), que aún siendo un magnífico plugin no ofrecía todas las características que necesitábamos. Aún así su trabajo ha sido completamente imprescindible para la realización de este plugin.
+
+= Más información =
+En nuestro sitio web oficial puede obtener más información sobre [**Custom Base Terms**](http://www.artprojectgroup.es/plugins-para-wordpress/custom-base-terms). 
+
+= Comentarios =
+No olvides dejarnos tu comentario en:
+
+* [Art Project Group](http://www.artprojectgroup.es/plugins-para-wordpress/custom-base-terms).
+* [Art Project Group](https://www.facebook.com/artprojectgroup) en Facebook.
+* [@artprojectgroup](https://twitter.com/artprojectgroup) en Twitter.
+* [+ArtProjectGroupES](https://twitter.com/artprojectgroup) en Google+.
+
+= Más plugins =
+Recuerda que puedes encontrar más plugin para WordPress en [Art Project Group](http://www.artprojectgroup.es/plugins-para-wordpress/) y en nuestro perfil en [WordPress](http://profiles.wordpress.org/artprojectgroup/).
 
 == Installation ==
-
-1. Subir la carpeta `custom-base` al directorio `/wp-content/plugins/`.
-2. Activar el plugin a través del menú 'Plugins' en el Panel de Administración de WordPress.
-3. Configurar el plugin en Ajustes -> Enlaces permanentes.
+1. Subir la carpeta `custom-base` al directorio `/wp-content/plugins/`, o subir el archivo ZIP completo vía *Plugins -> Añadir nuevo -> Subir* en el Panel de Administración de tu instalación de WordPress.
+2. Activar el plugin a través del menú *Plugins* en el Panel de Administración de WordPress.
+3. Configurar el plugin en *Ajustes -> Enlaces permanentes*.
+4. Listo, ahora ya puedes disfrutar de él, y si te gusta y te resulta útil, hacer una [*donación*](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EWFS5SMZ5UYU4).
 
 == Frequently asked questions ==
-
 = ¿Cómo se configura? =
-
-Sólo hay que ir a Ajustes -> Enlaces permanentes e indicar las nuevas estructuras personalizadas en las URLs para autor, búsqueda, comentarios y página, al igual que para categoría y etiqueta.
+Sólo hay que ir a *Ajustes -> Enlaces permanentes* e indicar las nuevas estructuras personalizadas en las URLs para autor, búsqueda, comentarios y página, al igual que para categoría y etiqueta.
 
 == Screenshots ==
-
-1. Captura de pantalla de Custom Base Terms.
+1. Captura de pantalla de **Custom Base Terms**.
 
 == Changelog ==
-
-= 0.1 =
-Versión inicial
-
+= 0.3 =
+Inclusión de traducciones. 
 = 0.2 =
-Versión inicial mejorada
+Versión inicial mejorada.
+= 0.1 =
+Versión inicial.
 
 == Upgrade Notice ==
-
+= 0.3 =
+Pequeñas modificaciones de código, inclusión de enlaces y actualización de los textos de información.
 = 0.2 =
-Arreglo de pequeños errores en el código fuente
+Arreglo de pequeños errores en el código fuente.
+
+==Traducciones ==
+* English
+* Español
+
+== ¿Por qué está esta página en español? ==
+Mientras WordPress no nos permita a los desarrolladores realizar esta página en más de un idioma, elegiremos siempre el español.
+
+A pesar de que es una apuesta muy arriesgada y de que reduce mucho las posibilidades de propagación de nuestros plugins, creemos que la comunidad hispana de WordPress es lo suficientemente amplia como para abocarla al idioma inglés hasta el final de los tiempos.
+
+Por ello regalamos a esa gran comunidad hispana nuestros plugins con interfaces, instrucciones, tutoriales, soporte y páginas web en *WordPress.org* en español.
+
+Esperamos que os guste nuestra iniciativa.
+
+== Donación ==
+¿Te ha gustado y te ha resultado útil **Custom Base Terms** en tu sitio web? Te agradeceríamos una [pequeña donación](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=EWFS5SMZ5UYU4) que nos ayudará a seguir mejorando este plugin y a crear más plugins totalmente gratuitos para toda la comunidad WordPress.
+
+== Gracias ==
+* A [Jeff Farthing](http://profiles.wordpress.org/jfarthing84/) por su gran plugin que ha inspirado **Custom Base Terms**.
+* A todos los que lo usáis.
+* A todos los que ayudáis a mejorarlo.
+* A todos los que realizáis donaciones.
+* A todos los que nos animáis con vuestros comentarios.
+
+¡Muchas gracias a todos!
